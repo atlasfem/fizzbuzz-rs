f1d228ef23a10508188cc4a7f6d1405bc7868766
atlasfem <modernatlas@yandex.com>
2020-02-08 04:50:01 -0600

    added junk

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+pub fn main() 
+
+{
+    
+    for x in 1..101 {
+    	if x %3 == 0 && x %5 == 0{
+    		println!("fizzbuzz");
+    	}
+    	else if x %3 == 0{
+    		println!("fizz");
+    	}
+    	else if x %5 == 0{
+    		println!("buzz");
+    	}
+    	else{
+    	println!("{}", x);
+    	}
+    }
+
+
+}
